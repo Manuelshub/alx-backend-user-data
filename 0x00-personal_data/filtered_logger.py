b0834f9e3a7f52775f1e31a70b21f0388a8b8561
Emmanuel <kcemmy03@gmail.com>
2024-08-30 10:35:45 +0100

    Function that returns the log message obfuscated

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Module contains a function that returns a log message obfuscated
+"""
+import re
+from typing import List
+
+
+def filter_datum(fields: List, redaction: str, message: str,
+                seperator: str) -> str:
+    """
+    Returns a log message obfuscated
+    
+    Arguments:
+        fields (List): a list of strings representing all fields to obfuscate
+        redaction (List): a list of strings representing the redaction values
+        message (str): the log entry to obfuscate
+        seperator (str): a character to be used as a separator between fields
+
+    Returns:
+        str: the obfuscated log entry
+    """
+    pattern = r"({})=([^{}]*)".format('|'.join(fields), seperator)
+    return re.sub(pattern, r"\1={}".format(redaction), message)
