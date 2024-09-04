8d8a3e9216067ebc8af4e628f588483e339f146b
Emmanuel <kcemmy03@gmail.com>
2024-09-04 14:24:23 +0100

    Credated a method that decodes base64 encoded string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,8 @@
 """ This module contains  a class for API Authentication
 """
 from .auth import Auth
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -27,3 +29,26 @@ class BasicAuth(Auth):
             return None
         else:
             return authorization_header[6:]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """
+        Decodes the Base64 part of the Authorization header.
+
+        Args:
+            base64_authorization_header (str):
+                The Base64 part of the Authorization header.
+
+        Returns:
+            str: The decoded value of the Base64 part of the
+                Authorization header.
+        """
+        if base64_authorization_header is None or type(
+             base64_authorization_header) is not str:
+            return None
+        try:
+            val = base64.b64decode(base64_authorization_header)
+        except binascii.Error:
+            return None
+        return val.decode('utf-8')
