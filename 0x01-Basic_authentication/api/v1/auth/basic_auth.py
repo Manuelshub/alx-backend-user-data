5a2a1fc175862a6fd824290075370687091c259b
Emmanuel <kcemmy03@gmail.com>
2024-09-04 12:52:57 +0100

    Created a class that inherits from the Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" This module contains  a class for API Authentication
+"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """ This class inherits from the Auth class
+    """
+    pass
