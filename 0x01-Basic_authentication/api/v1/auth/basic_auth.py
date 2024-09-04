d575d9bc8338c7922338aa9ed606640ba206ea95
Emmanuel <kcemmy03@gmail.com>
2024-09-04 13:21:43 +0100

    Added a methid that extracts the base64 part of the Authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,22 @@ from .auth import Auth
 class BasicAuth(Auth):
     """ This class inherits from the Auth class
     """
-    pass
+    
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Extracts the Base64 part of the Authorization header.
+
+        Args:
+            authorization_header (str): The Authorization header.
+
+        Returns:
+            str: The Base64 part of the Authorization header.
+        """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if authorization_header[:6] != 'Basic ':
+            return None
+        else:
+            return authorization_header[6:]
