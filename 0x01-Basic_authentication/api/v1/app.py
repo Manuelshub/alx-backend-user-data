6929fbd4a10952551266d7ef72e61e0f9714ac1c
Emmanuel <kcemmy03@gmail.com>
2024-09-04 12:32:40 +0100

    Added a method that checks if a request is authenticated or not

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,11 +7,16 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+if getenv('AUTH_TYEP') == 'auth':
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -33,6 +38,23 @@ def forbidden(error) -> str:
     """ Forbidden
     """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def before_request():
+    """
+    Check if the request is authenticated or not.
+    If the request is authenticated, it will store the current user
+    in the flask.g object.
+    """
+    auth_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                  '/api/v1/forbidden/']
+
+    if auth and auth.require_auth(request.path, auth_paths):
+        if not auth.authorization_header(request):
+            abort(401)
+        if not auth.current_user(request):
+            abort(403)
 
 
 if __name__ == "__main__":
