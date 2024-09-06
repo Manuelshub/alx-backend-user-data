cc6d165f377001440a05979037459423c6a1becb
Emmanuel <kcemmy03@gmail.com>
2024-09-06 11:18:50 +0100

    Modified the before request function

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -49,11 +49,16 @@ def forbidden(error) -> str:
 def before_request() -> None:
     """ Filter for request
     """
-    auth_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                  '/api/v1/forbidden/']
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
+    ]
 
-    if auth and auth.require_auth(request.path, auth_paths):
-        if auth.authorization_header(request) is None:
+    if auth and auth.require_auth(request.path, excluded_paths):
+        if auth.authorization_header(request) is None and \
+         auth.session_cookie(request) is None:
             abort(401)
         request.current_user = auth.current_user(request)
         if auth.current_user(request) is None:
