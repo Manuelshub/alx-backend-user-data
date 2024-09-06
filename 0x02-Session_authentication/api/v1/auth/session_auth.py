79efe434570f333ad1d70edbb65eb9aec36394f2
Emmanuel <kcemmy03@gmail.com>
2024-09-06 09:38:56 +0100

    Created a class for session authentication

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" Module contains a class that inherits from Auth.
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """ This class inherits from Auth class.
+    """
+    pass
