7cd7a1d742ddf655b1047ceebc8d90c04d0edaab
Emmanuel <kcemmy03@gmail.com>
2024-09-11 19:27:38 +0100

    Created a class that authenticates and registers a new user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """ This module contains a method that hashes a string.
 """
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 import bcrypt
 
 
@@ -19,3 +22,32 @@ def _hash_password(password: str) -> bytes:
     hashed_pwd = bcrypt.hashpw(byte_pwd, hash_salt)
 
     return hashed_pwd
+
+
+class Auth:
+    """Auth class to interact with the authentication database
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register a new user in the database.
+
+        Args:
+            email (str): The user's email address
+            password (str): The user's password
+
+        Returns:
+            User: The new User instance
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            user = None
+        else:
+            raise ValueError(f"User {email} already exists")
+        hash_pwd = _hash_password(password)
+        new_user = self._db.add_user(email, hash_pwd)
+        return new_user
