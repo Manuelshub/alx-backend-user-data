44c9ab4e544912927ead678ecf0ae53bd8889a9d
Emmanuel <kcemmy03@gmail.com>
2024-09-12 21:48:12 +0100

    Remodified return statement

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -88,7 +88,7 @@ class Auth:
         try:
             user = self._db.find_user_by(email=email)
         except NoResultFound:
-            return None
+            return
         sess_id = _generate_uuid()
         self._db.update_user(user.id, session_id=sess_id)
         return sess_id
