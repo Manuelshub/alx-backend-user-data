161d17453f7a4c64a7d886c675745b8db39d85f5
Emmanuel <kcemmy03@gmail.com>
2024-09-13 05:07:34 +0100

    Added / to the endpoint route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -84,7 +84,7 @@ def get_reset_password_token() -> str:
         return jsonify({"email": email, "reset_token": token}), 200
 
 
-@app.route('reset_password', methods=['PUT'], strict_slashes=False)
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password() -> str:
     """ update user password
     """
